e281cdee09cc2cc920c5b30548e3a46112704a18
mychanak <alice.uniyhkust2015@gmail.com>
2018-02-21 02:40:41 +0800

    Combine 2 py into one

--- Main.py
+++ Main.py
@@ -1,53 +1,136 @@
-# 5.1 Collection Dropping and Empty Collection Creating
-def display_message():
-    print("Collection dropping and empty colecction creating are successful")
-    return
+# coding=utf-8
+# The program was written by Juliana KO and Alice Chan.
+# The program is used for illustrating how to write a program with stubs using the "bank" application
+#
+def dropCollection():
+    print ("Collection dropping and empty collection creating are successful")
 
 
 # 5.2 Data Crawling
-def crawling(str):
-    if str == "default":
-        print("Data Crawling is successful and all data are inserted into database")
+def crawlData():
+    data = raw_input("Please input the URL/Keywords: ")
+    if (data == "default"):
+        choice = "keyword"
     else:
-        print("Data Crawling is successful and all data are inserted into database")
+        choice = "url"
+    print ("Data Crawling is successful and all data are inserted into the database")
 
 
-# 5.3 Course Search
+# 5.3.0 Print Course
+# For each distinct course, show “Course Code”, “Course Title”, “No. of Units/Credits”, “Matched Time Slot”
+# For the list of sections (including both lecture sections and non-lecture sections) of the course, show “Section”, “Date & Time”, “Quota”, “Enrol”, “Avail”, “Wait” and “Satisfied”
+
+def printSection(section, condition3):
+
+    if condition3 == "":
+        title_list = ["Section", "Date & Time", "Quota","Enroll","Avail","Wait"]
+        row_format = "{:>10} {:>15} {:>10} {:>10} {:>10} {:>10} "
+        print (
+            row_format.format(title_list[0], title_list[1], title_list[2], title_list[3], title_list[4], title_list[5]))
+        print("   --------------------------------------------------------------------")
+        print  (row_format.format(section[0], section[1], section[2], section[3], section[4], section[5]))
+
+    else:
+        title_list = ["Section", "Date & Time", "Quota", "Enroll", "Avail", "Wait", "Satisfied"]
+        row_format = "{:>10} {:>15} {:>10} {:>10} {:>10} {:>10} {:>10} "
+        print (row_format.format(title_list[0], title_list[1], title_list[2], title_list[3], title_list[4], title_list[5], title_list[6]))
+        print("   ----------------------------------------------------------------------------------")
+        print  (row_format.format(section[0], section[1], section[2], section[3], section[4], section[5], section[6]))
+
+
+# for
+def printCourse(course,condition3):
+    title_list = ["Course Code", "Course Title", "No. of Credit"]
+    row_format = "{:>20} {:>20} {:>20}"
+    print ("")
+    print (row_format.format(title_list[0],title_list[1],title_list[1]))
+    print ("==========================================================================")
+    print (row_format.format(course[0],course[1],course[2]))
+    print ("")
+
+    if condition3 =="":
+        print ('{:>42}'.format("Sections"))
+    else:
+        print ('{:>44}'.format("Matched Time Slot "))
+        course[3].append("Yes")
+    printSection(course[3], condition3)
+
+
 # 5.3.1 Course Search by Keyword
-def search_by_keyword(word):
-    if word == "match":
-        print ("extracted from database")
-        return "success"
-    else:
-        return
+def searchByKeyword():
+    keywords = raw_input("Please input the Keyword(s): ")
+
+    section = ["L1", "Tue 10:30", 40, 30, 10, 8]
+    course = ("COMP4332", "Big Data Mining", 3 ,section)
+    printCourse(course, "")
 
 
 # 5.3.2 Course Search by Waiting List Size
-def search_by_list_szie(num, start_ts, end_ts):
-    if num == 0 and start_ts == 0 and end_ts == 0:
-        print ("extracted from database")
-        return "success"
-    else:
-        return
+# only extract course from database which statisfy the match_ts
+def searchBySize():
+    size = input("Please input the Maximum Waiting List Size: ")
+    start_ts = input("Please input the Starting Time Slot: ")
+    end_ts = input("Please input the Ending Time Slot: ")
+
+    section = ["L1", "Tue 10:30", 40, 30, 10, 8]
+    course = ("COMP4332", "Big Data Mining", 3 ,section )
+
+    printCourse(course, size)
+
+
+# 5.3 Course Search
+def courseSearch():
+    choice3 = 0
+    while (choice3 != 3):
+        print("")
+        print("   Course Search Menu")
+        print("=======================")
+        print("1. Course Search by Keyword")
+        print("2. Course Search by Waiting List Size")
+        print("3. Exit")
+        print("")
+
+        # allow the user to choose one of the functions in the menu
+        choice3 = input("Please input your choice (1-3): ")
+
+        print("")
+
+        # check the input and call the correspondence function
+        if (choice3 == 1):
+            searchByKeyword()
+        elif (choice3 == 2):
+            searchBySize()
+        elif (choice3 == 3):
+            print("")
+        else:
+            print("Invalid Input!")
 
 
 # 5.4 Waiting List Size Prediction
-def predict(course_code, lecture_no, time_slot):
+def prediction():
+    course_code = raw_input("Please input the Course Code: ")
+    lecture_no = input("Please input the Lecture Number (e.g. the input should be “1” denoting “L1”): ")
+    time_slot = input("Please input the Time Slot: ")
     print ("Search course that match " + course_code + ", " + str(lecture_no) + " and " + str(time_slot))
-    predict_list = [0, 0, 0, 0, 0]
-    return predict_list
+    N1 = 10
+    N2 = 20
+    N3 = 30
+    N4 = 40
+    N5 = 50
+    print ("Prediction: "+str(N1) +", " + str(N2) + ", "+ str(N3) +", " +str(N4) +", " + str(N5))
 
 
-# 5.5
+# 5.5 Waiting List Size Training
 def training():
     print ("Waiting list size training is successful")
 
 
-# to display the interface
+# 5.0 to display the interface
 def main():
+    # here, we need to implement for the flow
     # display the menu
     choice = 0
-    while choice != 6:
+    while (choice != 6):
         print("")
         print("   Main Menu")
         print("=======================")
@@ -61,63 +144,22 @@ def main():
 
         # allow the user to choose one of the functions in the menu
         choice = input("Please input your choice (1-6): ")
+
         print("")
 
         # check the input and call the correspondence function
-        if choice == 1:
-            display_message()
-        elif choice == 2:
-            url = raw_input("Enter a URL or 'default' for crawling: ")
-            crawling(url)
-        elif choice == 3:
-            option = 0
-            while option != 3:
-                print("")
-                print("   Course Search")
-                print("=======================")
-                print("1. Course Search by Keyword")
-                print("2. Course Search by Waiting List Size")
-                print("3. Exit")
-                print("")
-
-                option = input("Please input your option (1-3): ")
-                if option == 1:
-                    keyword = raw_input("Please input your keyword: ")
-                    course_list = search_by_keyword(keyword)
-                    if course_list is not None:
-                        print(
-                            "Course Code    Course Title    No. of Units/Credits      Section     Date & Time     "
-                            "Quota   Enrol   Avail   Wait")
-                    else:
-                        print("No Courses Match the " + keyword)
-                elif option == 2:
-                    multiplier = -1
-                    while multiplier < 0:
-                        multiplier = input("Please input a multiplier: ")
-                    start_ts = input("Please input the starting time slot: ")
-                    end_ts = input("Please input the ending time slot: ")
-                    course_list = search_by_list_szie(multiplier, start_ts, end_ts)
-                    if course_list is not None:
-                        print(
-                            "Course Code    Course Title    No. of Units/Credits      Section     Date & Time     "
-                            "Quota   Enrol   Avail   Wait   Satisified")
-                    else:
-                        print("No Courses Matches")
-                elif option == 3:
-                    print("")
-                else:
-                    print ("Invalid Input")
-        elif choice == 4:
-            course_code = raw_input("Please input the course code: ")
-            lecture_no = input("Please input the lecture no.:")
-            time_slot = input("Please input the time slot: ")
-            predict_list = predict(course_code, lecture_no, time_slot)
-            print ("The prediction of the course: " + str(predict_list))
-
-        elif choice == 5:
+        if (choice == 1):
+            dropCollection()
+        elif (choice == 2):
+            crawlData()
+        elif (choice == 3):
+            courseSearch()
+        elif (choice == 4):
+            prediction()
+        elif (choice == 5):
             training()
-        elif choice == 6:
-            exit()
+        elif (choice == 6):
+            print("")
         else:
             print("Invalid Input!")
 
